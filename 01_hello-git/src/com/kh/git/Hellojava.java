c2ae6cc3f5b07f1bc6a0e6cf90c69b7bcb1515ec
jisoo274 <govkfl02@gmail.com>
2022-05-09 10:22:50 +0900

    hello java init!

---
+++ 01_hello-git/src/com/kh/git/Hellojava.java
@@ -0,0 +1,8 @@
+package com.kh.git;
+
+public class Hellojava {
+	public static void main(String[] args) {
+		System.out.println("Hello java");
+		
+	}
+}
--- 01_hello-git/src/com/kh/git/Helloworld.java
+++ 01_hello-git/src/com/kh/git/Helloworld.java
@@ -18,5 +18,8 @@ public class Helloworld {
  */
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		
 	}
+	
+	
 }
